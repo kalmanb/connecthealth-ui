99ad4d2eee999fdd5c6018c3523d8c7523634de0
kalmanb <kalman@movio.co>
2015-05-31 15:52:29 +1200

    Change to new router

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 
 
+### Build
+
+    bower install
+    npm install
+    cd bower_components/angular-new-router
+    cp .bower.json bower.json
+    # Add main line
+    "main": "dist/router.es5.min.js"
 
 ### TODO
 
 * new ng router
+* es6
 * jade
 * search patients
 * firebase
--- src/app/index.js
+++ src/app/index.js
@@ -1,20 +1,37 @@
 'use strict';
 
-import MainCtrl from './main/main.controller';
+import MainCtrl from '../app/components/main/main.controller';
 import NavbarCtrl from '../app/components/navbar/navbar.controller';
 
-angular.module('connecthealthApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ngRoute', 'ngMaterial'])
+
+function dashCase(str) {
+    return str.replace(/([A-Z])/g, function ($1) {
+        return '-' + $1.toLowerCase();
+    });
+}
+function componentLoaderConfig($componentLoaderProvider) {
+    // Load components from scr/app/components instead of ./components
+    $componentLoaderProvider.setTemplateMapping(function (name) {
+        var dashName = dashCase(name);
+        // customized to use app prefix
+        return './app/components/' + dashName + '/' + dashName + '.html';
+    });
+    // Use NameCtrl instead of NameController
+    $componentLoaderProvider.setCtrlNameMapping(function (name) {
+        return name[0].toUpperCase() + name.substr(1) + 'Ctrl';
+    });
+}
+
+function AppCtrl($router) {
+    $router.config([
+        { path: '/', component: 'main' },
+    ]);
+}
+
+angular.module('app', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ngNewRouter', 'ngMaterial' ])
   .controller('MainCtrl', MainCtrl)
   .controller('NavbarCtrl', NavbarCtrl)
+  .controller('AppCtrl', ['$router', AppCtrl])
+  .config(componentLoaderConfig);
 
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
-;
+
